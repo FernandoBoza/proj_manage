8e8262cd465ea5b8519ea87e99dde56d3c264eea
Fernando Boza <hi@fernandoboza.com>
2019-10-07 23:11:58 -0400

    user can now update password, TODO create notifications for updated info

--- dashboard/src/app/components/pages/settings/setting_components/change-password/change-password.component.ts
+++ dashboard/src/app/components/pages/settings/setting_components/change-password/change-password.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/User';
 import {UserServiceService} from "../../../../../services/user-service.service";
+import {CompanyService} from "../../../../../services/company.service";
 
 @Component({
   selector: 'change-password',
@@ -8,7 +9,10 @@ import {UserServiceService} from "../../../../../services/user-service.service";
 })
 export class ChangePasswordComponent implements OnInit {
 
-  constructor(private us: UserServiceService) { }
+  constructor(
+    private us: UserServiceService
+  ) { }
+
   public user: User = this.us.user;
   public newPassword: string = "";
   public confirmPassword: string = "";
@@ -19,6 +23,13 @@ export class ChangePasswordComponent implements OnInit {
 
   public updatePassword() {
     this.confirm = this.confirmPassword == this.newPassword;
+
+    if (this.confirm) {
+      this.user.password = this.newPassword;
+      this.us.updateUserPassword(this.user).subscribe(res => {
+        this.us.user = res;
+      })
+    }
   }
 
   public getValFromTxt(e?: string, name?: string) {
